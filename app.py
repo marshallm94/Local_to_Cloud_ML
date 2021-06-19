d6d10ddc79ac92674cedb9f11d15113d41f10a00
Marshall McQuillen <marshallm94@gmail.com>
2021-06-18 20:23:11 -0500

    Add Dockerfile - can get prediction from container on local machine

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+import numpy as np
+from pickle import load
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+model_file = 'prod_model.joblib'
+prod_model = load(open( model_file, 'rb' ))
+
+@app.route('/', )
+def index():
+    return "you suck"
+
+@app.route('/get_predictions/', methods=['POST'])
+def predict(model=prod_model):
+    if request.method == 'POST':
+        data = json.loads( request.data )
+        X = pd.read_json(data)
+        predictions = np.array2string( model.predict(X) )
+        return jsonify( predictions )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
