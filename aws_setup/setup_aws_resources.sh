ee511b71a68b35474b7c70f6e84ceb7464c12e69
Marshall McQuillen <marshallm94@gmail.com>
2021-07-29 13:30:40 -0500

    intial commit (of ALB creation) - I do in fact need an IGW

--- aws_setup/setup_aws_resources.sh
+++ aws_setup/setup_aws_resources.sh
@@ -16,7 +16,41 @@ docker tag ml-server $repo_url
 docker push $repo_url
 
 ################################################################################
-# Create Fargate Cluster
+# Configure Networking
+################################################################################
+# attach IGW to VPC
+igw_id=`aws ec2 create-internet-gateway | grep InternetGatewayId | sed s/^.*\://g | sed s/\"//g | sed s/,//g`
+vpc_id=`aws ec2 describe-vpcs | grep VpcId | sed s/^.*\://g | sed s/\"//g | sed s/,//g`
+aws ec2 attach-internet-gateway \
+    --vpc-id $vpc_id \
+    --internet-gateway-id $igw_id
+
+
+# choose two subnets/AZ's for the application
+subnet_1_id=`aws ec2 describe-subnets | grep SubnetId | head -n 1 | sed s/^.*\://g | sed s/\"//g | sed s/,//g`
+subnet_2_id=`aws ec2 describe-subnets | grep SubnetId | head -n 2 | tail -n 1 | sed s/^.*\://g | sed s/\"//g | sed s/,//g`
+
+aws elbv2 create-target-group help \
+    --name MLServerTargetGroup \
+    --target-type ip
+
+# creating security group and configuring to forward traffic from the IGW --> Load Balancer
+aws ec2 create-security-group \
+    --group-name FargateMLServerLoadBalancer-SecurityGroup \
+    --description "Security group for the load balancer used by FargateMLServerCluster" > create_security_group_output.json
+load_balancer_security_group_id=`python -c 'import json; obj=json.load(open("create_security_group_output.json","r"));print(obj["GroupId"])'`
+aws ec2 authorize-security-group-ingress \
+    --group-id $load_balancer_security_group_id \
+    --protocol tcp \
+    --port 80 \
+    --cidr 0.0.0.0/0 > /dev/null
+aws elbv2 create-load-balancer help \
+    --load-balancer-name MLServerLoadBalancer \
+    --subnets $subnet_1_id $subnet_2_id \
+    --security-groups $load_balancer_security_group_id
+
+################################################################################
+# Create Fargate Service
 ################################################################################
 # create IAM service role for Fargate cluster
 aws iam create-role \
@@ -52,6 +86,7 @@ aws ecs create-service \
     --task-definition $task_definition \
     --desired-count 1 \
     --launch-type "FARGATE" \
+    --load-balancer FILL_ME_IN \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-32c0d06f],securityGroups=[$security_group_id],assignPublicIp=ENABLED}" > ecs_create_service_output.json
 
 echo "Waiting for service to be created..."
@@ -83,3 +118,4 @@ rm ecs_cluster_output.json
 rm ecs_create_service_output.json
 rm task_definition_output.json
 echo 'Done'
+
